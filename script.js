17fa90ab88375d8eb78fb5fb71ae633293c0e065
Wallef Amorim <wallef720p@gmail.com>
2022-11-17 11:11:09 -0300

    Change Format of images

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+
+//Slide inicial de Banners
+//Variaveis utilizadas
 let time = 3000,
     indexC = 0,
     currentImageIndex = 0,
     imagens = document.querySelectorAll("#slide img"),
     max = imagens.length;
 
+    //Função que inicia o slide
 function start() {
     setInterval(() => {
         nextImage()
     }, time)
 }
 
+// Função que faz a imagens passarem 
 function nextImage() {
 
     imagens[currentImageIndex].classList.remove("imgfocus")
@@ -23,6 +28,7 @@ function nextImage() {
     imagens[currentImageIndex].classList.add("imgfocus")
 }
 
+// aplicando a funcionalidade de passar imagens nos botões
 let btLeft = document.querySelector("#left")
 let btRight = document.querySelector("#right")
 
@@ -54,9 +60,11 @@ function bfrButton() {
     imagens[currentImageIndex].classList.add("imgfocus")
 }
 
+// Area de funções relacionados aos produtos
 let timeMove = 1500;
 let intervalo;
 
+//Função de slide de imagens dos produtos
 function minhaFuncao(e) {
     let limit = e.children.length;
 
@@ -76,10 +84,12 @@ function minhaFuncao(e) {
 
 console.log(intervalo)
 
+//Para o slide do produto
 function stopMove() {
     clearInterval(intervalo)
 }
 
+//Aréa de seleção das categorias de produtos
 let buttonSecond = document.querySelector("#mai"),
     buttonFirst = document.querySelector("#biq"),
     buttonThird = document.querySelector("#sai");
@@ -97,31 +107,32 @@ let posBiquini = document.querySelector(".biquinis").getBoundingClientRect(),
 //console.log(posMaio)
 //console.log(posSaida)
 
+//seleção da categoria de biquini
 function moveBiquini() {
 
-    if (posiCategoria.top > 0) {
         categoria.scrollBy({
-            top: 0,
+            top: -2260,
             behavior: 'smooth'
         });
-    } else {
-        console.log("já esta na posição");
-    }
+    
 }
+console.log(categoria.DOCUMENT_POSITION_CONTAINED_BY)
 
+//seleção da categoria de Maio
 function moveMaio() {
 
     categoria.scrollBy({
-        top: 1100,
+        top: 1120,
         behavior: 'smooth'
     });
     console.log(posiCategoria);
 }
 
+//seleção da categoria de Saida de praia
 function moveSaida() {
 
     categoria.scrollBy({
-        top: 2200,
+        top: 2260,
         behavior: 'smooth'
     });
     console.log(posiCategoria);
